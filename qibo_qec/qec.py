0643a8fc3cd1eafa2ac92de267835e029aed5638
Yedi278 <yehan278@gmail.com>
2025-11-18 23:57:45 +0100

    phase flip implementation on qec class

--- qibo_qec/qec.py
+++ qibo_qec/qec.py
@@ -23,6 +23,10 @@ class QEC:
 
             case "bit_flip":
                 return self.bit_flip_code(circuit, test_gates, correction)
+            case "phase_flip":
+                return self.phase_flip_code(circuit, test_gates, correction)
+            case _:
+                raise NotImplementedError(f"Code type {self.code_type} not supported yet.")
 
         return self.encoded_circuit
 
@@ -107,11 +111,9 @@ class QEC:
         #     self.encoded_circuit.add(gates.CZ(i*5+2, i*5+4))
         #     self.encoded_circuit.add(gates.H(i*5+4))
 
-        # Aplly corrections based on syndrome measurements
-
         if correction:
             for i in range(circuit.nqubits):
-                # Correction based on ancilla measurements
+
                 self.encoded_circuit.add(gates.CNOT(i*3, i*3+1))
                 self.encoded_circuit.add(gates.CNOT(i*3, i*3+2))
                 self.encoded_circuit.add(gates.TOFFOLI(i*3+1, i*3+2, i*3))
@@ -122,28 +124,29 @@ class QEC:
                 self.encoded_circuit.add(gates.M(target*3))
 
         return self.encoded_circuit
+    
+
+    def phase_flip_code(self, circuit:Circuit, test_gates:list=None, correction:bool=True) -> Qec_Circuit:
         """Applies the Phase-Flip QEC code to the given quantum circuit."""
 
-        assert self.code_type == "phase_flip", "Code type must be 'phase_flip' to use this method."
-
-        self.encoded_nqb = circuit.nqubits * 3 + 2 * circuit.nqubits
-        print(f"Applying {self.code_type} code to a circuit with {self.encoded_nqb} qubit(s).")
+        self.encoded_nqb = circuit.nqubits * 3
+        if self.log: print(f"Applying {self.code_type} code to a circuit with {self.encoded_nqb} qubit(s).")
 
         # Create new wire names for the encoded circuit
         self.wire_names = []
         for i in range(circuit.nqubits):
-            for j in range(3):
-                self.wire_names.append(f"q{i}{j}")
-            self.wire_names.append(f"a{i}0")
-            self.wire_names.append(f"a{i}1")
+            self.wire_names.append(f"q_{i}")
+            self.wire_names.append(f"a_{i}0")
+            self.wire_names.append(f"a_{i}1")
 
         # Initialize the encoded circuit
         self.encoded_circuit = Qec_Circuit(circuit=circuit, nqubits=self.encoded_nqb, wire_names=self.wire_names)
 
         # Encoding: Apply CNOT gates to encode each qubit into three qubits
         for i in range(circuit.nqubits):
-            self.encoded_circuit.add(gates.CNOT(i*5, i*5+1))
-            self.encoded_circuit.add(gates.CNOT(i*5, i*5+2))
+            self.encoded_circuit.add(gates.CNOT(i*3, i*3+1))
+            self.encoded_circuit.add(gates.CNOT(i*3, i*3+2))
+            self.encoded_circuit.add( [ gates.H(i*3), gates.H(i*3+1), gates.H(i*3+2) ])
 
         # add test error gates if provided
 
@@ -160,28 +163,28 @@ class QEC:
                 
                 case "x":
                     target = gate["_target_qubits"][0]
-                    self.encoded_circuit.add(gates.X(target*5))
-                    self.encoded_circuit.add(gates.X(target*5+1))
-                    self.encoded_circuit.add(gates.X(target*5+2))
+                    self.encoded_circuit.add(gates.X(target*3))
+                    self.encoded_circuit.add(gates.X(target*3+1))
+                    self.encoded_circuit.add(gates.X(target*3+2))
 
                 case "z":
                     target = gate["_target_qubits"][0]
-                    self.encoded_circuit.add(gates.Z(target*5))
-                    self.encoded_circuit.add(gates.Z(target*5+1))
-                    self.encoded_circuit.add(gates.Z(target*5+2))
+                    self.encoded_circuit.add(gates.Z(target*3))
+                    self.encoded_circuit.add(gates.Z(target*3+1))
+                    self.encoded_circuit.add(gates.Z(target*3+2))
 
                 case "h":
                     target = gate["_target_qubits"][0]
-                    self.encoded_circuit.add(gates.H(target*5))
-                    self.encoded_circuit.add(gates.H(target*5+1))
-                    self.encoded_circuit.add(gates.H(target*5+2))
+                    self.encoded_circuit.add(gates.H(target*3))
+                    self.encoded_circuit.add(gates.H(target*3+1))
+                    self.encoded_circuit.add(gates.H(target*3+2))
 
                 case "cx":
                     control = gate["_control_qubits"][0]
                     target = gate["_target_qubits"][0]
-                    self.encoded_circuit.add(gates.CNOT(control*5, target*5))
-                    self.encoded_circuit.add(gates.CNOT(control*5+1, target*5+1))
-                    self.encoded_circuit.add(gates.CNOT(control*5+2, target*5+2))
+                    self.encoded_circuit.add(gates.CNOT(control*3, target*3))
+                    self.encoded_circuit.add(gates.CNOT(control*3+1, target*3+1))
+                    self.encoded_circuit.add(gates.CNOT(control*3+2, target*3+2))
                 
                 case "measure":
                     self.meas_target.append(gate['_target_qubits'][0])  # Store measurement target for later
@@ -190,43 +193,18 @@ class QEC:
                 case _:
                     print(f"Gate {gate['name']} not supported in bit-flip code yet.")
 
-        # Stabilizer measurements: Measure syndromes using ancilla qubits
-        for i in range(circuit.nqubits):
-
-            # Measure Z0Z1
-            self.encoded_circuit.add(gates.H(i*5+3))
-            self.encoded_circuit.add(gates.CZ(i*5, i*5+3))
-            self.encoded_circuit.add(gates.CZ(i*5+1, i*5+3))
-            self.encoded_circuit.add(gates.H(i*5+3))
-
-            # Measure Z1Z2
-            self.encoded_circuit.add(gates.H(i*5+4))
-            self.encoded_circuit.add(gates.CZ(i*5+1, i*5+4))
-            self.encoded_circuit.add(gates.CZ(i*5+2, i*5+4))
-            self.encoded_circuit.add(gates.H(i*5+4))
-
-        # Aplly corrections based on syndrome measurements
-
         if correction:
             for i in range(circuit.nqubits):
-                # Correction based on ancilla measurements
-                self.encoded_circuit.add(gates.CNOT(i*5+3, i*5+1))
-                self.encoded_circuit.add(gates.CNOT(i*5+3, i*5+2))
-
-                self.encoded_circuit.add(gates.CNOT(i*5+4, i*5))
-                self.encoded_circuit.add(gates.CNOT(i*5+4, i*5+1))
-                
-                self.encoded_circuit.add(gates.X(i*5))
-                self.encoded_circuit.add(gates.X(i*5+1))
-                self.encoded_circuit.add(gates.X(i*5+2))
-
-
+    
+                self.encoded_circuit.add( [gates.H(i*3), gates.H(i*3+1), gates.H(i*3+2)])
+                self.encoded_circuit.add(gates.CNOT(i*3, i*3+1))
+                self.encoded_circuit.add(gates.CNOT(i*3, i*3+2))
+                self.encoded_circuit.add(gates.TOFFOLI(i*3+1, i*3+2, i*3))
 
         # Final measurements if the original circuit had measurements
         if self.meas_target:
             for target in self.meas_target:
-                self.encoded_circuit.add(gates.M(target*5))
-                self.encoded_circuit.add(gates.M(target*5+1))
-                self.encoded_circuit.add(gates.M(target*5+2))
+                self.encoded_circuit.add(gates.M(target*3))
 
         return self.encoded_circuit
+    
